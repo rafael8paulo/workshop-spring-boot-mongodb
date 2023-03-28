ded2fa3cb85f8ca083d022849de4c561c98ab13c
Rafael Oliveira <rafael8paulo@gmail.com>
2023-03-27 23:58:03 -0300

    Consulta com varios criterios

--- src/main/java/com/rpx/workshopmongo/service/PostService.java
+++ src/main/java/com/rpx/workshopmongo/service/PostService.java
@@ -4,9 +4,11 @@ import com.rpx.workshopmongo.domain.Post;
 import com.rpx.workshopmongo.domain.User;
 import com.rpx.workshopmongo.repository.PostRepository;
 import com.rpx.workshopmongo.service.exception.ObjectNotFoundException;
+import org.apache.tomcat.jni.Pool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,7 +23,13 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
     public List<Post> findByTitle(String text){
-//        return repository.findByTitleContainingIgnoreCase(text);
+        //return repository.findByTitleContainingIgnoreCase(text);
         return repository.searchTitle(text);
     }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
+    }
+
 }
